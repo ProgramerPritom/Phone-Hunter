b75966d964510a60eddf58e62474ba1382369682
ProgramerPritom <badruzzamanpritom@gmail.com>
2022-03-01 19:36:21 +0600

    how many value result added

--- app.js
+++ app.js
@@ -14,8 +14,30 @@ const getSearch = () => {
 
         // Load Phone api
         const loadPhone = () => {
-            
+            const url = `https://openapi.programming-hero.com/api/phones?search=${searchField.value}`;
+            fetch(url)
+            .then(res => res.json())
+            .then(data => displayData(data.data))
+            .catch(error => displayError(error))
+        };
+        const displayError = error =>{
+
+        };
+        const resultValue = document.getElementById('search-result');
+        const displayData = data =>{
+            const displayApi = document.getElementById('display-api');
+            if (data.length > 1) {
+                resultValue.innerText = `${data.length} found for ${searchField.value}`;
+            }
+            else if(data.length == 1){
+                resultValue.innerText = `${data.length} found for ${searchField.value}`;
+            }
+            data.forEach(phoneData=>{
+                // console.log(phoneData);
+                
+            });
         }
+        loadPhone();
 
 
 
