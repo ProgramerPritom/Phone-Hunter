a7b7fe745996fa3500c35318cd6d50814639d2f5
ProgramerPritom <badruzzamanpritom@gmail.com>
2022-03-01 19:59:45 +0600

    undefined value return solve

--- app.js
+++ app.js
@@ -18,20 +18,25 @@ const getSearch = () => {
             fetch(url)
             .then(res => res.json())
             .then(data => displayData(data.data))
-            .catch(error => displayError(error))
+            
         };
-        const displayError = error =>{
-
-        };
+        
         const resultValue = document.getElementById('search-result');
         const displayData = data =>{
             const displayApi = document.getElementById('display-api');
             if (data.length > 1) {
-                resultValue.innerText = `${data.length} found for ${searchField.value}`;
+                resultValue.innerText = `${data.length} Result found for ${searchField.value}`;
             }
             else if(data.length == 1){
-                resultValue.innerText = `${data.length} found for ${searchField.value}`;
+                resultValue.innerText = `${data.length} Result found for ${searchField.value}`;
             }
+            else if(data == ""){
+                let noResult = `${searchField.value} Result not Found`;
+                document.getElementById('search-result').innerText = noResult;
+            }
+
+
+
             data.forEach(phoneData=>{
                 // console.log(phoneData);
                 
